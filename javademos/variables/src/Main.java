7183f7439b28d60ac6a721d4639b06d7ac84a083
Mert ÖRNEK <mertornek2000@gmail.com>
2022-09-21 19:34:15 +0300

    Java Kampı 2022
    Engin Demiroğ ile java kampı 2022

---
+++ javademos/variables/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Oğrenci sayım : "+ ogrenciSayisi);
+		System.out.println("Oğrenci sayım : "+ ogrenciSayisi);
+		System.out.println("Oğrenci sayım : "+ ogrenciSayisi);
+		
+
+	}
+
+}
